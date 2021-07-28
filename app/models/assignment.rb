18d9cce736d6460d98d890d2e7df56dd0f0aeeab
kevn7n <kevn_nguyen@yahoo.com>
2021-07-27 18:06:22 -0600

    Add validations & relationships to model spec tests

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,6 @@
 class Assignment < ApplicationRecord
+  validates :completed, presence: true
+
   belongs_to :roommate
   belongs_to :chore
 end
--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Assignment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:chore) }
+    it { should belong_to(:roommate) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:completed) }
+  end
 end
