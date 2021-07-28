2e97a635669efcf511006c85965bb366dc5efe99
Suzie K <suziekim.dev@gmail.com>
2021-07-27 21:42:05 -0400

    Add missing validations

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,7 @@
 class Chore < ApplicationRecord
   validates :task_name, :description, :outdoor, presence: true
   validates :weight, presence: true, numericality: true
+  validates :frequency, presence: true, numericality: true
 
   belongs_to :household
   has_many :assignments
--- spec/services/openweather_service_spec.rb
+++ spec/services/openweather_service_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe OpenWeatherService do
         VCR.use_cassette("open_weather_forecast") do
           coordinates = { lat: 35, lon: 127 }
           response = OpenWeatherService.get_forecast_data(coordinates)
-          require 'pry'; binding.pry
+          
           expect(response).to be_a(Hash)
           expect(response[:lat]).to eq(coordinates[:lat])
           expect(response[:lon]).to eq(coordinates[:lon])
