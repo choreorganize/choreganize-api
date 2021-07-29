3bf5b149985f0c8d4b937e98df33ee66b2356071
Suzie K <suziekim.dev@gmail.com>
2021-07-28 21:53:35 -0400

    Green test cannot find user

--- app/controllers/api/v1/roommates_controller.rb
+++ app/controllers/api/v1/roommates_controller.rb
@@ -6,7 +6,14 @@ class Api::V1::RoommatesController < ApplicationController
   end
 
   def show
+    user = Roommate.find(params[:id])
+    render json: RoommatesSerializer.new(user), status: 200
 
+  rescue ActiveRecord::RecordNotFound
+    render json: {
+      message: 'Not Found',
+      errors: ["Could not find user with id##{params[:id]}"]
+    }, status: :not_found
   end
 
   def create
