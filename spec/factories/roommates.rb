2377d52fc6598f86b2ee4cc43a9ea60fbde4a6b8
Suzie K <suziekim.dev@gmail.com>
2021-07-28 21:38:13 -0400

    Update factories

--- spec/factories/roommates.rb
+++ spec/factories/roommates.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
-  factory :roommate, class: Roommate do
-    user_id { nil }
-    household { nil }
+  factory :mock_roommate, class: Roommate do
+    name { Faker::FunnyName.name }
+    email { Faker::Internet.email }
+    google_id { Faker::Internet.password }
+    token { Faker::Internet.password }
+    
+    association :household, factory: :mock_household
   end
 end
