0846fd5f2d3eb9a6214d980fd77f892f3d739d18
Zach T <ztrokey@gmail.com>
2021-07-28 20:08:06 -0600

    add create test for roommate

--- spec/requests/api/v1/roommates/roommates_request_spec.rb
+++ spec/requests/api/v1/roommates/roommates_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "User API" do
-  before(:all) do
-    @household = create(:mock_household)
-  end
+  # before(:all) do
+  #   @household = create(:mock_household)
+  # end
 
   context 'User index' do
     it 'happy path: sends all users' do
@@ -13,7 +13,7 @@ RSpec.describe "User API" do
 
       body = JSON.parse(response.body, symbolizes_names: true)
 
-      expect(response).to be_successful 
+      expect(response).to be_successful
       expect(body[:data].size).to eq(3)
     end
   end
@@ -22,7 +22,22 @@ RSpec.describe "User API" do
     it 'happy path: can find a single user'
     it 'sad path: cannot find the user'
 
-  context 'Users create'
-    it 'happy path: can create a new user'
-    it 'sad path: cannot create user without all params'  
+  context 'Users create' do
+    it 'happy path: can create a new user' do
+
+      post '/api/v1/roommates', params: {
+        roommate: {
+          name: 'Moe deGrasse Tyson',
+          email: 'cutthatgrass@ex.com',
+          google_id: '456',
+          token: '1234567890'
+        }
+      }
+
+      body = JSON.parse(response.body, symbolizes_names: true)
+      # require 'pry'; binding.pry
+      expect(response).to be_successful
+    end
+    it 'sad path: cannot create user without all params'
+  end
 end
