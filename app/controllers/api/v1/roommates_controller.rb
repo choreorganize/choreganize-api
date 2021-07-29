689f4726e84cb414c9ffea74dc51d8a05510aeaf
Zach T <ztrokey@gmail.com>
2021-07-28 20:07:48 -0600

    add create method and roommate_params

--- app/controllers/api/v1/roommates_controller.rb
+++ app/controllers/api/v1/roommates_controller.rb
@@ -9,6 +9,19 @@ class Api::V1::RoommatesController < ApplicationController
   end
 
   def create
+    # require 'pry'; binding.pry
+    roommate = Roommate.new(roommate_params)
 
+    if roommate.save
+      render json: roommate, status: :created
+    else
+      render json: roommate.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def roommate_params
+    params.require(:roommate).permit(:name, :email, :google_id, :token)
   end
 end
