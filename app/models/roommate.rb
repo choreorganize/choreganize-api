6a4591f359851e84c1adafec59d4aeff79e66d65
Zach T <ztrokey@gmail.com>
2021-07-28 21:16:07 -0600

    add optional association to allow nil value

--- app/models/roommate.rb
+++ app/models/roommate.rb
@@ -2,9 +2,9 @@ class Roommate < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :google_id, presence: true
-  validates :token, presence: true 
-  
-  belongs_to :household
+  validates :token, presence: true
+
+  belongs_to :household, optional: true
   has_many :assignments
   has_many :chores, through: :assignments
 end
