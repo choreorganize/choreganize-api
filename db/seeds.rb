490e60163be24913536575b709650e886fc52e80
Suzie K <suziekim.dev@gmail.com>
2021-07-28 20:45:18 -0400

    Setup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Household.destroy_all
+Roommate.destroy_all 
+Chore.destroy_all
+
+20.times do 
+  Household.create do |household| 
+    household.address = Faker::Address.street_address
+    household.city = "denver"
+    household.state = "co"
+    household.housecode_digest = Faker::Internet.password
+    3.times do 
+      household.roommates.new do |roommate|
+        roommate.name = Faker::FunnyName.name
+        roommate.email = Faker::Internet.email
+        roommate.google_id = Faker::Internet.password
+        roommate.token = Faker::Internet.password
+      end
+    end
+    9.times do 
+      household.chores.new do |chore|
+        chore.task_name = Faker::Hobby.activity 
+        chore.description = Faker::Hipster.paragraph
+        chore.weight = [1,2,3].sample
+        chore.frequency = [1,2,3].sample
+        chore.outdoor = [true,false].sample
+      end
+    end
+  end
+end
