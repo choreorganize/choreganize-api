b7ba10db8b04f17d0b66b2ed11d21545d8d82abd
Suzie K <suziekim.dev@gmail.com>
2021-07-29 20:39:24 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :roommates, except: [:new, :edit, :destroy]
-      resources :chores, only: [:index, :show, :create, :update, :destroy]
+      resources :chores, except: [:new, :edit]
     end
   end
 end
