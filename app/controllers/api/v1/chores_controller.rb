07557d072a4003f1077660110650f829405fefe8
Suzie K <suziekim.dev@gmail.com>
2021-07-29 20:39:09 -0400

    Green test can create a new chore

--- app/controllers/api/v1/chores_controller.rb
+++ app/controllers/api/v1/chores_controller.rb
@@ -14,14 +14,27 @@ class Api::V1::ChoresController < ApplicationController
       message: 'Not Found',
       errors: ["Could not find chore with id##{params[:id]}"]
     }, status: :not_found
-
   end
 
   def create
-
+    chore = Chore.new(chore_params)
+    
+    if chore.save
+      render json: ChoresSerializer.new(chore), status: 201
+    else      
+      render json: {
+      message: 'Invalid',
+      errors: chore.errors.map { |_attr, msg| msg }
+    }, status: 422
+    end
   end
 
   def edit
 
   end
+
+  private
+    def chore_params 
+      params.require(:chore).permit(:task_name, :weight, :frequency, :household_id, :description, :outdoor)
+    end
 end
