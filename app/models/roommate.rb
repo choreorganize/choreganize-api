71f40d6f043901a3202e333bd4e63165fc0e8d87
Suzie K <suziekim.dev@gmail.com>
2021-08-01 21:06:45 -0400

    Green test incomplete chores

--- app/models/roommate.rb
+++ app/models/roommate.rb
@@ -7,4 +7,10 @@ class Roommate < ApplicationRecord
   belongs_to :household, optional: true
   has_many :assignments
   has_many :chores, through: :assignments
+  
+  def incomplete_chores
+    chores.joins(:assignments)
+    .select("chores.* as chores")
+    .where('assignments.completed = true')
+  end
 end
