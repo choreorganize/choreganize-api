071426d7277e8290a1a3be2db34cd4daa976625c
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-08-01 18:37:18 -0600

    get 'create household' working

--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe Household, type: :model do
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
-    it { should validate_presence_of(:housecode_digest) }
+    it { should validate_presence_of(:password_digest) }
   end
 end
---
+++ spec/requests/api/v1/household/create_request_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe "Household create  API" do
+
+  context 'happy paths' do
+    it ' returns household information' do
+      post '/api/v1/household', params: {
+        household: {
+          address: '22b baker st',
+          city: 'metropolis',
+          state: 'key stone',
+          password: 'super secret',
+          password_confirmation: 'super secret'
+        }
+      }
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(body[:data]).to have_key(:id)
+      expect(body[:data]).to have_key(:type)
+      expect(body[:data][:type]).to eq('households')
+
+      expect(body[:data]).to have_key(:attributes)
+      expect(body[:data][:attributes][:address]).to eq('22b baker st')
+      expect(body[:data][:attributes][:city]).to eq('metropolis')
+      expect(body[:data][:attributes][:state]).to eq('key stone')
+
+      expect(body[:data][:attributes][:chores]).to eq([])
+    end
+
+    it ' adds something to data base' do
+
+      test = Household.find_by(address: '22b baker st')
+      # binding.
+      expect(test).to eq(nil)
+
+
+      post '/api/v1/household', params: {
+        household: {
+          address: '22b baker st',
+          city: 'metropolis',
+          state: 'key stone',
+          password: 'super secret',
+          password_confirmation: 'super secret'
+        }
+      }
+
+      test_2 = Household.find_by(address: '22b baker st')
+
+      expect(test_2.class).to eq(Household)
+    end
+
+  end
+
+end
