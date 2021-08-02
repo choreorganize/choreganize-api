35c11d0fc6d7168e1f46f1866bbe41364d161108
Suzie K <suziekim.dev@gmail.com>
2021-08-01 22:07:52 -0400

    Update Open weather service method

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,7 +1,7 @@
 class OpenWeatherService
 
   def self.get_forecast_data(coordinates)
-    new.request_api("/data/2.5/onecall?lat=#{coordinates[:lat]}&lon=#{coordinates[:lon]}&exclude=minutely,hourly,alerts")
+    new.request_api("/data/2.5/onecall?lat=#{coordinates[:lat]}&lon=#{coordinates[:lng]}&exclude=minutely,hourly,alerts")
   end
 
   def request_api(path)
