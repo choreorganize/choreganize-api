200eca40118f3a2fed6f1cf4e8b3a367c14a80d5
Suzie K <suziekim.dev@gmail.com>
2021-08-02 18:26:51 -0400

    Migrate weather_forecast method to household

--- app/models/household.rb
+++ app/models/household.rb
@@ -5,4 +5,11 @@ class Household < ApplicationRecord
   has_many :chores
 
   has_secure_password
+
+  # is this violating a design pattern?
+  def weather_forecast 
+    city = self.city
+    state = self.state
+    ForecastFacade.forecast(city, state)
+  end
 end
--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Household, type: :model do
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:password_digest) }
   end
+
+  describe '#weather_forecast' do
+    it 'returns weather forecast' do
+      household = create(:mock_household)
+
+      expect(household.weather_forecast).to be_a(ForecastDetails)
+    end
+  end
 end
