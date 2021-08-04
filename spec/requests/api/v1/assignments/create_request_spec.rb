d4790f693d833ec2bd2b6aede21edb30534c6974
Suzie K <suziekim.dev@gmail.com>
2021-08-04 17:53:35 -0400

    Green all assignment create test

--- spec/requests/api/v1/assignments/create_request_spec.rb
+++ spec/requests/api/v1/assignments/create_request_spec.rb
@@ -1,34 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Assignment create API" do
-  # before(:all) do
-  #   # @household = Household.find(161)
-  #   # @chore = Chore.find(240)
-  #   # @roommate = Roommate.find(130)
-  #   @household = Household.create!()
-  # end
+  before(:all) do
+    household = Household.create!(address: Faker::Address.street_address, city: 'denver', state: 'co', password_digest: Faker::Internet.password)
+    @chore = Chore.create!({
+        id: 1,
+        task_name: 'Mow',
+        household_id: household.id,
+        description: 'Cut some grass, my friend.',
+        weight: 1,
+        frequency: 'weekly',
+        outdoor: true
+      })
+ 
+    @roommate = Roommate.create!(id: 2, name: Faker::FunnyName.name, email: Faker::Internet.email, google_id: Faker::Internet.password, token: Faker::Internet.password, household: household)
+  end
 
   context 'happy paths' do
-    it 'returns assignment information' do
-      household = Household.create!(address: Faker::Address.street_address, city: 'denver', state: 'co', password_digest: Faker::Internet.password)
-      chore = 
-      chore1 = Chore.create!({
-            id: 1,
-            task_name: 'Mow',
-            household_id: household.id,
-            description: 'Cut some grass, my friend.',
-            weight: 1,
-            frequency: 'weekly',
-            outdoor: true
-          })
-     
-      roommate = Roommate.create!(id: 2, name: Faker::FunnyName.name, email: Faker::Internet.email, google_id: Faker::Internet.password, token: Faker::Internet.password, household: household)
-     
+    it 'returns assignment information' do     
       post '/api/v1/assignments', params: {
         assignment: {
           completed: false,
-          roommate_id: roommate.id,
-          chore_id: chore.id
+          roommate_id: @roommate.id,
+          chore_id: @chore.id
         }
       }
 
@@ -41,110 +35,103 @@ RSpec.describe "Assignment create API" do
 
       expect(body[:data]).to have_key(:attributes)
       expect(body[:data][:attributes][:completed]).to eq(false)
-      expect(body[:data][:attributes][:roommate_id]).to eq(roommate.id)
-      expect(body[:data][:attributes][:chore_id]).to eq(chore.id)
+      expect(body[:data][:attributes][:roommate_id]).to eq(@roommate.id)
+      expect(body[:data][:attributes][:chore_id]).to eq(@chore.id)
+    end
+
+    it 'adds something to data base' do
+      Assignment.destroy_all
+      test = Assignment.find_by(roommate_id: @roommate.id)
+      
+      expect(test).to eq(nil)
+
+      post '/api/v1/assignments', params: {
+        assignment: {
+          completed: false,
+          roommate_id: @roommate.id,
+          chore_id: @chore.id
+        }
+      }
+
+      test_2 = Assignment.find_by(roommate_id: @roommate.id)
+      
+      expect(test_2.class).to eq(Assignment)
+    end
+  end
+
+   context 'sad paths' do
+     describe 'invalid roommate' do
+       describe 'No existing roommate' do
+         it 'returns errors' do
+           post '/api/v1/assignments', params: {
+             assignment: {
+               completed: false,
+               roommate_id: 0,
+               chore_id: @chore.id
+             }
+           }
+           body = JSON.parse(response.body, symbolize_names: true)
+          
+           expect(response.status).to eq(400)
+
+           expect(body).to have_key(:message)
+           expect(body).to have_key(:errors)
+           expect(body[:message]).to eq("Bad Request")
+           expect(body[:errors]).to eq("Cannot create assignment. Roommate must exist, Chore must exist")
+         end
+
+         it 'does not add anything to the data base' do
+
+           test = Assignment.find_by(roommate_id: 0)
+           expect(test).to eq(nil)
+           post '/api/v1/assignments', params: {
+             assignment: {
+               completed: false,
+               roommate_id: 0,
+               chore_id: @chore.id
+             }
+           }
+
+           test_2 = Assignment.find_by(roommate_id: 0)
+
+           expect(test_2.class).to eq(NilClass)
+         end
+       end
+     end
+
+     describe 'invalid chore' do
+       it 'returns errors' do
+         post '/api/v1/assignments', params: {
+          assignment: {
+             completed: false,
+             roommate_id: @roommate.id,
+             chore_id: 0
+           }
+         }
+         body = JSON.parse(response.body, symbolize_names: true)
+      
+         expect(response.status).to eq(400)
+         expect(body).to have_key(:message)
+         expect(body).to have_key(:errors)
+         expect(body[:message]).to eq("Bad Request")
+         expect(body[:errors]).to eq("Cannot create assignment. Roommate must exist, Chore must exist")
+       end
+       it 'does not add anything to the data base' do
+         Assignment.destroy_all 
+         test = Assignment.find_by(roommate_id: @roommate.id)
+         expect(test).to eq(nil)
+
+         post '/api/v1/assignments', params: {
+           assignment: {
+             completed: false,
+             roommate_id: @roommate.id,
+            chore_id: 0
+           }
+         }
+         test_2 = Assignment.find_by(roommate_id: @roommate.id)
+         expect(test_2.class).to eq(NilClass)
+       end
     end
   end
 end
-#     xit 'adds something to data base' do
-#       Assignment.destroy_all
-#       test = Assignment.find_by(roommate_id: @roommate.id)
-      
-#       expect(test).to eq(nil)
-
-#       post '/api/v1/assignments', params: {
-#         assignment: {
-#           completed: false,
-#           roommate_id: @roommate.id,
-#           chore_id: @chore.id
-#         }
-#       }
-
-#       test_2 = Assignment.find_by(roommate_id: @roommate.id)
-      
-#       expect(test_2.class).to eq(Assignment)
-#     end
-#   end
-
-#   context 'sad paths' do
-#     describe 'invalid roommate' do
-#       describe 'No existing roommate' do
-#         xit 'returns errors' do
-#           post '/api/v1/assignments', params: {
-#             assignment: {
-#               completed: false,
-#               roommate_id: 0,
-#               chore_id: @chore.id
-#             }
-#           }
-#           body = JSON.parse(response.body, symbolize_names: true)
-          
-#           expect(response.status).to eq(400)
-
-#           expect(body).to have_key(:message)
-#           expect(body).to have_key(:errors)
-#           expect(body[:message]).to eq("Bad Request")
-#           expect(body[:errors]).to eq("Cannot create assignment. Roommate must exist, Chore must exist")
-#         end
-
-#         xit 'does not add anything to the data base' do
-
-#           test = Assignment.find_by(roommate_id: 0)
-#           expect(test).to eq(nil)
-
-#           post '/api/v1/assignments', params: {
-#             assignment: {
-#               completed: false,
-#               roommate_id: 0,
-#               chore_id: @chore.id
-#             }
-#           }
-
-#           test_2 = Assignment.find_by(roommate_id: 0)
-
-#           expect(test_2.class).to eq(NilClass)
-#         end
-#       end
-#     end
-
-#     describe 'invalid chore' do
-#       xit 'returns errors' do
-#         post '/api/v1/assignments', params: {
-#           assignment: {
-#             completed: false,
-#             roommate_id: @roommate.id,
-#             chore_id: 0
-#           }
-#         }
-#         body = JSON.parse(response.body, symbolize_names: true)
-       
-#         expect(response.status).to eq(400)
-
-#         expect(body).to have_key(:message)
-#         expect(body).to have_key(:errors)
-#         expect(body[:message]).to eq("Bad Request")
-#         expect(body[:errors]).to eq("Cannot create assignment. Roommate must exist, Chore must exist")
-#       end
-
-#       xit 'does not add anything to the data base' do
-#         Assignment.destroy_all 
-
-#         test = Assignment.find_by(roommate_id: @roommate.id)
-#         expect(test).to eq(nil)
-
-#         post '/api/v1/assignments', params: {
-#           assignment: {
-#             completed: false,
-#             roommate_id: @roommate.id,
-#             chore_id: 0
-#           }
-#         }
-
-#         test_2 = Assignment.find_by(roommate_id: @roommate.id)
-
-#         expect(test_2.class).to eq(NilClass)
-#       end
-#     end
-#   end
-# end
  
