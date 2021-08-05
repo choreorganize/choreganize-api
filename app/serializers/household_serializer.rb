4affad49ece0a8da25616f40d9a9e8f95468e6d2
Suzie K <suziekim.dev@gmail.com>
2021-08-05 07:10:20 -0400

    Add id to attributes

--- app/serializers/household_serializer.rb
+++ app/serializers/household_serializer.rb
@@ -1,5 +1,5 @@
 class HouseholdSerializer
   include JSONAPI::Serializer
   set_type :households
-  attributes :address, :city, :state, :roommates, :chores, :weather_forecast
+  attributes :id :address, :city, :state, :roommates, :chores, :weather_forecast
 end
